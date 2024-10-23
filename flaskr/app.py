1c917e16589349c8596868ef0c99d58511143763
Amanda Pereira Sampaio <amandapersampa@hotmail.com>
2024-10-23 10:54:14 +0100

    new design

--- flaskr/app.py
+++ flaskr/app.py
@@ -6,5 +6,8 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-app.run(host="0.0.0.0", port=80)
+@app.route("/acomodacao")
+def acomodacao():
+    return render_template("index.html")
 
+app.run(host="0.0.0.0", port=80)
